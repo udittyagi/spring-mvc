4fad545f265ca8f7c77e2561b26f5b80c7de8c76
udittyagi <udittyagi2@gmail.com>
2017-09-19 13:59:33 +0530

    Spring MVC Examples

---
+++ src/main/java/com/udit/controllers/ProductController.java
@@ -0,0 +1,29 @@
+package com.udit.controllers;
+
+import com.udit.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by udit on 19/9/17.
+ */
+@Controller
+public class ProductController {
+
+    private ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @RequestMapping("/products")
+    public String listProducts(Model model){
+
+        model.addAttribute("products", productService.listAllProducts());
+
+        return "products";
+    }
+}
---
+++ src/main/java/com/udit/domain/Product.java
@@ -0,0 +1,45 @@
+package com.udit.domain;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by udit on 19/9/17.
+ */
+public class Product {
+    private Integer id;
+    private String description;
+    private BigDecimal price;
+    private String imageUrl;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
